2e05afd94a7ca34f2399194fe0dc0dd45744f5a5
devops <devops@gmail.com>
2024-05-22 17:08:29 +0000

    updated f1.sh in master

--- f1.sh
+++ f1.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-echo enter the file to be searched
+#echo enter the file to be searched
+#read file
+#if [ -f "$file" ]
+#then
+#echo $file exists
+#else
+#echo $file does not exists
+#fi
+
+echo enter the file to be removed
 read file
 if [ -f "$file" ]
 then
-echo $file exists
+rm $file
+echo $file removed
 else
-echo $file does not exists
+echo $file not removed
 fi
+
+
 
